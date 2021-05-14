94534de18edb0ccd29ee5fe9e29ef8aeb2399bc5
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-14 12:59:26 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Come to Turkland, and ............... all these places where history lives on.",
+        options:["discover", "inform", "invent", "memorize", "mention"],
+        answer: 0
+    },
+    {
+        q:"I'm ............... a speedy answer, i.e. within a few days.",
+        options:["excepting", "expecting", "accepting"],
+        answer: 1
+    },
+    {
+        q:"Stamp collecting is one of the world's most popular ................   .",
+        options:["customs", "institutions", "hobbies", "traditions", "usages"],
+        answer: 2
+    },
+    {
+        q:"How much do I ............... you?",
+        options:["owe", "owen to"],
+        answer: 0
+    },
+    {
+        q:"How much do you ............... it? How much is that in Turkish liras?",
+        options:["accept", "sell", "purchase", "expect", "charge for"],
+        answer: 4
+    },
+    {
+        q:"Don't you ............... any interest in national politics?",
+        options:["take", "acquire", "give", "remember", "bring"],
+        answer: 0
+    },
+    {
+     q:"People with no previous ............... need not apply.",
+        options:["experiment", "experience", "inexperience", "experiments", "experimentation"],
+        answer: 1   
+    },
+    {
+        q:"His doctor told him that he must stay away from hard work and take it easy for a couple of weeks after such a serious ...............   .",
+        options:["disease", "epidemic", "illness", "recovery", "prescription"],
+        answer: 2
+    },
+    {
+        q:"I ............... to receiving your letters day by day with ever-growing impatience.",
+        options:["wait anxiously", "get disappointed", "expect cheerfully", "look forward", "eagerly hope"],
+        answer: 3
+    },
+    {
+        q:"A lot of people lose their jobs in times of an economic depression and ............... quickly increases to record levels.",
+        options:["employment", "production", "consumption", "investments", "unemployment"],
+        answer: 4
+    }
+]
